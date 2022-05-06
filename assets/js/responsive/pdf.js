e80fd355333b36d59a2fc5a9e35f407a70c7d4f2
AdrianOrtiga <adrian.ortiga@gmail.com>
2022-05-06 09:35:54 +0800

    fix responsive CV pdf and, remove the teacher thing

--- assets/js/responsive/pdf.js
+++ assets/js/responsive/pdf.js
@@ -2,21 +2,20 @@ const resumeArticle = document.getElementById('resume')
 const MIN_PDF_SIZE = 900
 
 const observer = new ResizeObserver(entries => {
-    const article = entries[0]
-    const pdf = resumeArticle.querySelectorAll('.embed-pdf')
-    const iconPdf = resumeArticle.querySelectorAll('.icon-pdf')
-    
-    if(article.contentRect.width > MIN_PDF_SIZE){
-        // show pdf 
-        pdf.hidden = false
-        iconPdf.hidden = true
-        return 
-    }
-
-    // show icon 
-    iconPdf.hidden = false
-    pdf.hidden = true
+  const article = entries[0]
+  const pdf = resumeArticle.querySelector('.embed-cv-pdf')
+  const iconPdf = resumeArticle.querySelector('.icon-cv-pdf')
+  if (article.contentRect.width > MIN_PDF_SIZE) {
+    // show pdf 
+    pdf.hidden = false
+    iconPdf.hidden = true
     return
+  }
+
+  // show icon 
+  iconPdf.hidden = false
+  pdf.hidden = true
+  return
 })
 
 observer.observe(resumeArticle)
